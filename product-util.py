92f6ce27e3341df17c4d229727365782a343c2c8
DeniseKim9063DeniseKim <t-nosova130558340@rambler.ru>
2025-12-04 12:50:29 +0800

    add decorator-upload-registry.txt, ZamaOracleAddress.sol, responsive-lib-interface.java

---
+++ product-util.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\product-util.py
+// Generated at: 2025-10-10T11:13:56.026Z
---
+++ src/responsive-lib-interface.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\src\responsive-lib-interface.java
+// Generated at: 2025-10-10T11:13:56.124Z
