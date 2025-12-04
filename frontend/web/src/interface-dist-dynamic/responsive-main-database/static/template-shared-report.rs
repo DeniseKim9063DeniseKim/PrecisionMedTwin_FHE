aa7f7a10e5aa2f41b8ee9448ec94ada6bfb82ba3
DeniseKim9063DeniseKim <t-nosova130558340@rambler.ru>
2025-12-04 12:50:52 +0800

    add index.ts, pages-constants.md, main-auth-user.toml

---
+++ frontend/web/src/interface-dist-dynamic/responsive-main-database/static/template-shared-report.rs
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\interface-dist-dynamic\responsive-main-database\static\template-shared-report.rs
+// Generated at: 2025-10-10T11:13:56.156Z
